567fad9cc5e9017d0d506d9237afc58d751bdf87
Walid <83774096+ziliwalid@users.noreply.github.com>
2023-03-20 09:20:40 +0000

    spring security done

--- src/main/java/com/example/apiapp/repository/GameRepository.java
+++ src/main/java/com/example/apiapp/repository/GameRepository.java
@@ -5,6 +5,8 @@ import com.example.apiapp.Model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GameRepository extends JpaRepository<Game,Long> {
 }
